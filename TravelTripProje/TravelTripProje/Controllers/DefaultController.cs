267e65fe144802d06b45eb5f32e7284316852886
Gani Ozturk <ganioztrk@gmail.com>
2023-05-12 14:04:24 +0300

    ders 28 sonu

--- TravelTripProje/TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/TravelTripProje/Controllers/DefaultController.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Entities;
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        private readonly Context _context = new Context();
         public ActionResult Index()
         {
-            return View();
+            var values = _context.Blogs.ToList();
+            return View(values);
+        }
+        public PartialViewResult Partial1()
+        {
+            var values = _context.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(values);
+        }
+
+            
+        public PartialViewResult Partial2()
+        {
+            var values = _context.Blogs.Where(x=>x.ID == 1).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var values = _context.Blogs.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Partial4()
+        {
+            var values = _context.Blogs.Take(3).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Partial5()
+        {
+            var values = _context.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+            return PartialView(values);
         }
     }
 }
